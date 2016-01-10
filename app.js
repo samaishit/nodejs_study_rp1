e82cb9d037e63ee0c6fe28d8a0f02517f60615d4
samaishit <samashi@gamail.com>
2016-01-10 17:24:14 +0900

    iconv-lite, jquery, jennifer-ui 설치 및 ui변경

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cheerio = require('cheerio');
 var request = require('request');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var _ = require("underscore");
 
 var app = express();
-
 var requestBaseOptionObject = {
     method:'GET',
 	  encoding:'binary'
@@ -36,6 +36,9 @@ var Post = mongoose.model('post', postSchema);
 //veiw setting
 app.set("view engine", 'ejs');
 
+//static directory setting
+app.use("/node_modules",express.static(__dirname + "/node_modules"));
+
 //set middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -47,18 +50,25 @@ app.get('/intro', function (req,res) {
                                 "key=4e483efff4f07ee183a9fabed8121687&target=news" +
                                 "&start=1&display=10&query=" +
                                 encodeURIComponent("부동산");
-  request(requestBaseOptionObject , function (error, response, body) {
+
+  requestEstateNewsUrl = "http://land.naver.com/news/headline.nhn";
+  request(requestEstateNewsUrl , function (error, response, body) {
                       if(!error && response.statusCode == 200) {
-                        var bufferedBody = new Buffer(body, 'binary');
+                        var bufferedBody = new Buffer(body, 'euckr');
                         var $ = cheerio.load(bufferedBody);
-                        var itemList = $('channel item');
-                        var dataArr = new Array();
+                        var itemList = $('#content .headline_list li dl');
+                        var dataArr = [];
                         itemList.each(function (v, element) {
+                          var titleObj = $(this).children('dt').length == 1 ?
+                                        $(this).children('dt')[0] :
+                                        $(this).children('dt')[1] ;
+
                           dataArr.push({
-                            title : $(this).children('title').html(),
-                            link : $(this).children('originallink').html(),
-                            desc : $(this).children('description').html(),
-                            date : $(this).children('pubDate').html()
+                            title : $(titleObj).children('a').html(),
+                            link : "http://land.naver.com" + $(titleObj).children('a').attr('href'),
+                            date : $(this).children('dd').children('.date').html(),
+                            desc : $(this).children('dd').remove('span').text()
+
                           });
                         });
 
